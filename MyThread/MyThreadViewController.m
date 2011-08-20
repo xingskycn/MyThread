340303cfd23d8f8fd3f7198826d2c5ae17fe7a50
Kazuo Koga <kogakazuo@gmail.com>
2011-08-21 01:07:27 +0900

    KAThread を使ってみるサンプル

--- MyThread/MyThreadViewController.m
+++ MyThread/MyThreadViewController.m
@@ -7,26 +7,56 @@
 //
 
 #import "MyThreadViewController.h"
+#import "KAThread.h"
+
+
+@interface MyThreadViewController ()
+
+@property(nonatomic, retain) KAThread *thread;
+
+@end
+
 
 @implementation MyThreadViewController
+@synthesize thread = thread_;
+
+- (void)hello:(id)_ {
+    NSLog(@"1 hello %@ %@", _, [NSRunLoop currentRunLoop]);
+}
+
+- (void)delayStop {
+    [self.thread performSelector:@selector(stopRunLoop) onThread:self.thread withObject:nil waitUntilDone:YES];
+    //[self.thread performSelector:@selector(requestStopRunLoop) withObject:nil];
+}
+
+- (void)delayHello {
+    NSLog(@"%s", __func__);
+    [self performSelector:@selector(hello:) onThread:self.thread withObject:@"End Test" waitUntilDone:NO];
+    NSLog(@"%s end", __func__);
+}
 
 - (void)didReceiveMemoryWarning
 {
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
-    
+
     // Release any cached data, images, etc that aren't in use.
 }
 
 #pragma mark - View lifecycle
 
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
+
+    self.thread = [[[KAThread alloc] init] autorelease];
+    [self.thread start];
+
+    [self performSelector:@selector(hello:) onThread:self.thread withObject:@"Async Test" waitUntilDone:NO];
+    [self performSelector:@selector(hello:) onThread:self.thread withObject:^{ NSLog(@"Sync Test"); } waitUntilDone:YES];
+
+    [self performSelector:@selector(delayStop) withObject:nil afterDelay:3.0f];
+    [self performSelector:@selector(delayHello) withObject:nil afterDelay:5.0f];
 }
-*/
 
 - (void)viewDidUnload
 {
